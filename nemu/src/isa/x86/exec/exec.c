03159bb3eb1d1f575df5b45501665ac3bb14dc73
Tokubara <1270802817@qq.com>
2021-06-17 20:23:46 +0800

    (feat,highlight) pass gold-bach 值得一提的是我以为的idiv与test(都是group3)的冲突问题, siyuan://blocks/20210617193925-fph0ay9

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -40,7 +40,7 @@ make_group(gp2,
 
 /* 0xf6, 0xf7 */
 make_group(gp3,
-    EMPTY, EMPTY, EMPTY, EMPTY,
+    IDEXW(test_I,test,1), EMPTY, EMPTY, EMPTY,
     EMPTY, EMPTY, EX(div), EX(idiv))
 
 /* 0xfe */
