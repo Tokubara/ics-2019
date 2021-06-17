374a32ebb8c0ccdd04ffe92f1be017581acc95bd
Tokubara <1270802817@qq.com>
2021-06-17 21:40:35 +0800

    (feat) load-store 100073:        0f b7 84 1b e0 01 10    movzwl 0x1001e0(%ebx,%ebx,1),%eax

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -174,7 +174,7 @@ static OpcodeEntry opcode_table[512] = {
   /* 0xa8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xac */	EMPTY, EMPTY, EMPTY, IDEX(E2G, imul2),
   /* 0xb0 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xb4 */	EMPTY, EMPTY, IDEX(mov_Eb2G, movzx), EMPTY,
+  /* 0xb4 */	EMPTY, EMPTY, IDEX(mov_Eb2G, movzx), IDEX(mov_Ew2G, movzx),
   /* 0xb8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xbc */	EMPTY, EMPTY, IDEX(mov_Eb2G, movsx), IDEX(mov_Ew2G, movsx),
   /* 0xc0 */	EMPTY, EMPTY, EMPTY, EMPTY,
