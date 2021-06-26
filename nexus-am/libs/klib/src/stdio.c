023a5106f8b7f2f53dd979ecc544a439c2b41eaa
Tokubara <1270802817@qq.com>
2021-06-26 17:20:59 +0800

    (refactor) fail hello-str: 试图修改sprintf的实现, 使得它能与printf共用

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,27 +3,50 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
+
+char int_buf[20]; // 最大的long long十进制长度为19
+// 返回长度
+int fill_int_buf(unsigned long long lval) {
+  int i = 0;
+  if(lval==0) {
+    int_buf[0]='0';
+    int_buf[1]='\0';
+    return 1;
+  }
+  char tmp;
+  while (lval > 0) {
+    int_buf[i++] = (lval % 10) + '0';
+    lval /= 10;
+  }
+  int left = 0;
+  int right = i - 1; // 现在i是右开, 因此-1
+  while (left < right) {
+    tmp = int_buf[right];
+    int_buf[right] = int_buf[left];
+    int_buf[left] = tmp;
+    ++left;
+    --right;
+  }
+  int_buf[i]='\0';
+  return i;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
-}
-
-int sprintf(char* out, const char* fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-
+int allprintf(char *out, const char *fmt, va_list ap) {
+#define out_putc(out_putc_ch)  if(out!=NULL) {\
+          out[i++] = out_putc_ch;\
+      } else {\
+        _putc(out_putc_ch);\
+      }
   size_t i = 0; // i指向out, j指向fmt
   const char* p;
   int ival;
-  long long lval;
-  char tmp;
+  unsigned long long lval = 0; // 为了过编
+  size_t int_buf_len;
+  char tmp_ch;
+  
   for (p = fmt; *p; ++p) {
     if (*p != '%') {
-      out[i] = *p;
-      ++i;
+      out_putc(*p);
       continue;
     }
 
@@ -32,32 +55,22 @@ int sprintf(char* out, const char* fmt, ...) {
     switch (*p) {
     case 's': {
       for (char* s = va_arg(ap, char*); *s; ++s) {
-        out[i] = *s;
-        ++i;
+        out_putc(*s);
       }
       break;
     }
     case 'd': {
       ival = va_arg(ap, int);
-      lval = ival;
-      if (lval < 0) {
-        out[i] = '-';
-        ++i;
-        lval = -lval;
+      if (ival < 0) {
+        out_putc('-');
+        lval = (-ival);
       }
-      int left = i;
-      while (lval > 0) {
-        out[i++] = (lval % 10) + '0';
-        lval /= 10;
+      int_buf_len = fill_int_buf(lval);
+      for(size_t i = 0; i<int_buf_len; i++) {
+        tmp_ch = int_buf[i];
+        out_putc(tmp_ch);
       }
-      int right = i - 1; // 现在i是右开
-      while (left < right) {
-        tmp = out[right];
-        out[right] = out[left];
-        out[left] = tmp;
-        ++left;
-        --right;
-      }
+      i+=int_buf_len;
       break;
     }
     default: {
@@ -68,6 +81,28 @@ int sprintf(char* out, const char* fmt, ...) {
 
   out[i] = '\0';
   return i;
+#undef out_putc
+}
+
+
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  allprintf(NULL, fmt, ap);
+  va_end(ap);
+  return 0;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  return 0;
+}
+
+int sprintf(char* out, const char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = allprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
