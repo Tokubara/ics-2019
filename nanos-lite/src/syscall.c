56b041d7f62590e029a7d02db092ad88c61ff1a1
Tokubara <1270802817@qq.com>
2021-07-01 20:16:23 +0800

    (feat) 3.2 添加对navy打印的支持, hello测例运行正常

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,8 +17,16 @@ _Context* do_syscall(_Context *c) {
                       break;
                     }
     case SYS_write: {
-                      _yield();
-                      c->GPR1 = 0;
+                      if (c->GPR2 == 1 || c->GPR2 == 2) {
+                        char* buf = (char*)c->GPR3;
+                        size_t len = c->GPR4;
+                        for(size_t i = 0; i < len; i++) {
+                          _putc(buf[i]);
+                        }
+                        c->GPR1 = c->GPR4;
+                      } else {
+                        printf("unsupport\n");
+                      }
                       break;
                     }
     default: panic("Unhandled syscall ID = %d", a[0]);
