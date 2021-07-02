077f575365d28d6ac3b10875046772d5ec4e0f0f
Tokubara <1270802817@qq.com>
2021-07-02 21:01:51 +0800

    (feat_untest) 3.3 添加fs_open等

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -3,6 +3,12 @@
 
 #include "common.h"
 
+int fs_open(const char *pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+ssize_t fs_lseek(int fd, ssize_t offset, int whence);
+int fs_close(int fd);
+
 #ifndef SEEK_SET
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
 #endif
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -9,6 +9,7 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset; // 添加的读写offset
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -25,13 +26,85 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, invalid_read, invalid_write},
+  {"stdin", 0, 0, invaid_read, invalid_write},
   {"stdout", 0, 0, invalid_read, invalid_write},
   {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
+
+int fs_open(const char *pathname, int flags, int mode) {
+  int fd = -1;
+  for(size_t i = 0; i < NR_FILES; i++) {
+    if(strcmp(file_table[i], pathname) == 0) {
+      fd = i;
+      break;
+    }
+  }
+  return fd;
+}
+#define min(a,b) (a<b?a:b)
+#define check_fd   if(fd < 0 || fd >= NR_FILES) {\
+    Log("error: invalid fd: %d", fd);\
+    return -1;\
+  }
+
+ssize_t fs_read(int fd, void *buf, size_t len) {
+  check_fd;
+  if(fd < 0 || fd >= NR_FILES) {
+    Log("error: invalid fd: %d", fd);
+    return 0;
+  }
+  size_t read_len = min(len, file_table[fd]->size - file_table[fd]->open_offset);
+  size_t disk_offset = file_table[fd]->disk_offset + file_table[fd]->open_offset;
+  ramdisk_read(buf, disk_offset, read_len);
+  file_table[fd]->open_offset += read_len;
+  return read_len;
+}
+
+ssize_t fs_write(int fd, const void *buf, size_t len) {
+  check_fd;
+  size_t write_len = min(len, file_table[fd]->size - file_table[fd]->open_offset);
+  size_t disk_offset = file_table[fd]->disk_offset + file_table[fd]->open_offset;
+  ramdisk_write(buf, disk_offset, write_len);
+  file_table[fd]->open_offset += write_len;
+  return write_len;
+}
+
+ssize_t fs_lseek(int fd, ssize_t offset, int whence) { // 讲义与man 2 lseek不一致, 是size_t, 应该是符号数类型
+  check_fd;
+  ssize_t new_offset = 0;
+  switch(whence) {
+    case SEEK_SET: {
+                     new_offset = offset;
+                     break;
+                   }
+    case SEEK_CUR: {
+                     new_offset = file_table[fd]->open_offset + offset;
+                     break;
+                   }
+    case SEEK_END: {
+                     new_offset = file_table[fd]->size + offset;
+                     break;
+                   }
+    default: {
+               Log("unknown whence: %d", whence);
+               new_offset = -1; // 表示不合法
+                     break;
+                   }
+  }
+  if(new_offset < 0 && new_offset > file_table[fd]->size) {
+    Log("invalid offset: %d, offset unchange", (int)new_offset);
+    return file_table[fd]->open_offset;
+  }
+  fd_table[fd]->open_offset = new_offset;
+  return new_offset;
+}
+
+int fs_close(int fd) {
+  return 0;
+}
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
