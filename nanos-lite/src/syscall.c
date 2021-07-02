39c7732568d4dba69343f1106a67ad2287fbe81f
Tokubara <1270802817@qq.com>
2021-07-02 22:10:26 +0800

    (feat) 3.3 添加syscall相应的项, pass了text测例

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "syscall.h"
+#include "fs.h"
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -16,6 +17,14 @@ _Context* do_syscall(_Context *c) {
                       c->GPRx = 0;
                       break;
                     }
+    case SYS_open: {
+                      c->GPRx = fs_open(c->GPR2, c->GPR3, c->GPR4);
+                      break;
+                   }
+    case SYS_read: {
+                      c->GPRx = fs_read(c->GPR2, c->GPR3, c->GPR4);
+                      break;
+                   }
     case SYS_write: {
                       if (c->GPR2 == 1 || c->GPR2 == 2) {
                         char* buf = (char*)c->GPR3;
@@ -25,8 +34,16 @@ _Context* do_syscall(_Context *c) {
                         }
                         c->GPRx = c->GPR4;
                       } else {
-                        printf("unsupport\n");
+                        c->GPRx = fs_write(c->GPR2, c->GPR3, c->GPR4);
                       }
+                      break;
+                    }
+    case SYS_close: {
+                      c->GPRx = fs_close(c->GPR2);
+                      break;
+                    }
+    case SYS_lseek: {
+                      c->GPRx = fs_lseek(c->GPR2, c->GPR3, c->GPR4);
                       break;
                     }
     case SYS_brk: {
