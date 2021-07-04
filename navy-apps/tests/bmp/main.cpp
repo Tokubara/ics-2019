cc3fa932a4a22d0842df3a545075d73a5b8da899
Tokubara <1270802817@qq.com>
2021-07-04 15:47:19 +0800

    (debug) 由于bmp中只输出了第一行, 但是第一行与第二行之间只有文件操作,怀疑是不是读文件有问题, 于是运行text测例, 发现总指令数为1031519

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -1,10 +1,13 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <ndl.h>
+#include <stdio.h>
 
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
+  printf("will load ficture\n");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
+  printf("load finish\n");
   assert(bmp->pixels);
   NDL_OpenDisplay(bmp->w, bmp->h);
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
