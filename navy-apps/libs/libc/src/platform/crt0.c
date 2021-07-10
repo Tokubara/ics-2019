cdf7aedf626b371e0af301a515868eda89845e43
Tokubara <1270802817@qq.com>
2021-07-11 06:53:34 +0800

    (debug) 用hello来测命令行参数

--- navy-apps/libs/libc/src/platform/crt0.c
+++ navy-apps/libs/libc/src/platform/crt0.c
@@ -6,6 +6,7 @@ extern char **environ;
 
 void _start(int argc, char *argv[], char *envp[]) {
   asm volatile ("movl %eax, %esp");
+	printf("[start]gc:%d, argv:%x, envp:%x, argv[0](%x):%s, argv[1](%x):%s\n", argc, argv, envp, argv[0], argv[0], argv[1], argv[1]);
   // char *env[] = {NULL};
   // environ = env;
   int ret = main(argc, argv, envp);
--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,16 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[], char *envp[]) {
+	printf("[main]argc:%d, argv:%x, envp:%x, argv[0](%x):%s, argv[1](%x):%s\n", argc, argv, envp, argv[0], argv[0], argv[1], argv[1]);
   write(1, "Hello World!\n", 13);
-  int i = 2;
-  volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    }
-  }
+  // int i = 2;
+  // volatile int j = 0;
+  // while (1) {
+  //   j ++;
+  //   if (j == 10000) {
+  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+  //     j = 0;
+  //   }
+  // }
   return 0;
 }
