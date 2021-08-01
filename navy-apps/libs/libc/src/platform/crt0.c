0bac644a71c4cfb0fccff6183e557ee4c66a660c
Tokubara <1270802817@qq.com>
2021-08-01 12:07:52 +0800

    (fix_untest) 修改了crt0:_start中关于esp的逻辑, 修改了_ucontext关于用户栈的逻辑

--- navy-apps/libs/libc/src/platform/crt0.c
+++ navy-apps/libs/libc/src/platform/crt0.c
@@ -6,14 +6,8 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 
 
-void _start() {
-  unsigned* esp;
-  asm volatile ("popl %%ebx; popl %%esi; popl %%edi; movl %%eax, %0; movl %%eax, %%esp; subl 4, %%esp; pushl %%edi; pushl %%esi; pushl %%ebx":"=r"(esp));
-  
-  int argc = esp[0];
-  char** argv = esp[1];
-  char** envp = esp[2];
-	printf("[start]argc:%d, argv:%x, envp:%x, argv[0](%x):%s, argv[1](%x):%s\n", argc, argv, envp, argv[0], argv[0], argv[1], argv[1]);
+void _start(int argc, char** argv, char** envp) {
+  printf("[start]argc:%d, argv:%x, envp:%x, argv[0](%x):%s, argv[1](%x):%s\n", argc, argv, envp, argv[0], argv[0], argv[1], argv[1]);
   char *env[] = {NULL};
   environ = env;
   int ret = main(argc, argv, envp);
