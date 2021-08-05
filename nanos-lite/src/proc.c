5c7b363c096cb926109003ec13f8bc73038d93b7
Tokubara <1270802817@qq.com>
2021-08-05 21:38:10 +0800

    (bug) 如果只有一个程序, 用开机程运行pal可以, 加上内核线程出错

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,14 +21,14 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  // context_kload(&pcb[0], hello_fun, "China");
+  context_kload(&pcb[0], hello_fun, "China");
   char* argv_0[3];
-  argv_0[0] = "/bin/litenes";
+  argv_0[0] = "/bin/init";
   argv_0[1] = "/share/games/nes/mario.nes";
   argv_0[2] = NULL;
   char* envp_0[1];
   envp_0[0] = NULL;
-  context_uload(&pcb[0], "/bin/litenes", argv_0, envp_0, 1, 0);
+  context_uload(&pcb[0], "/bin/init", argv_0, envp_0, 1, 0);
   char* argv_1[3];
   argv_1[0] = "/bin/dummy";
   argv_1[1] = "--skip";
